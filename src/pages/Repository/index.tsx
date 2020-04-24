04ae9e310299fadafa3fc5c04234885f5eb81741
Anderson Alves <andersoonalvess@gmail.com>
2020-04-23 23:02:53 -0300

    [ReactJS] Create repository detais page and styles.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues} from './styles';
+import logImg from '../../assets/logo.svg';
+import api from '../../services/api';
 
 interface RepositoryParams {
     repository: string;
@@ -9,7 +13,32 @@ const Repository: React.FC = () => {
 
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository: {params.repository}</h1>
+    return (
+        <>
+            <Header>
+                <img src={logImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+            Back
+            </Link>
+            </Header>
+            <RepositoryInfo>
+
+            </RepositoryInfo>
+
+            <Issues>
+            <Link key={repository.full_name}
+                    to={`/repositories/${repository.full_name}`}>
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+
+                        <FiChevronRight size={20} />
+                    </Link>
+            </Issues>
+        </>
+    );
 }
 
 export default Repository;
